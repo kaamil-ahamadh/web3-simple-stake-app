a4dbd1a22413f7f3dba6d1ce7fdc0a7808a72f0a
Kaamil Ahamadh <kaamilahamadh6@gmail.com>
2022-08-20 01:57:40 +0530

    Change Smart-Contracts Addresses

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,6 +2,6 @@ import Staking from "./Staking.json";
 import Token from "./Token.json";
 
 export const stakingABI = Staking.abi;
-export const stakingAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const stakingAddress = "0x948491d3f710adb870ee44545f1a57e6521188ce";
 export const tokenABI = Token.abi;
-export const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const tokenAddress = "0x569EB2A0f04A7134788d7c02b6B804D9B1d82D09";
